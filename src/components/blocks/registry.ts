67546791d996a29a1382ba95977a73b8927621a5
Hari <harisridhar77@gmail.com>
2026-08-29 22:21:15 +0530

    Last set of Blocks added + Basic header and footer

--- src/components/blocks/registry.ts
+++ src/components/blocks/registry.ts
@@ -1,7 +1,11 @@
 import Hero from "./Hero/Hero.astro";
 import Feature from "./Feature/Feature.astro";
+import Action from "./Action/Action.astro";
+import Testimonial from "./Testimonial/Testimonial.astro";
 
 export const blockRegistry = {
     hero: Hero,
     feature: Feature,
+    action: Action,
+    testimonial: Testimonial,
 } as const;
--- src/content/schema.ts
+++ src/content/schema.ts
@@ -2,6 +2,7 @@ import type { SchemaContext } from "astro:content";
 import { z } from "astro/zod";
 
 export const createPageSchema = ({ image }: Pick<SchemaContext, "image">) => {
+    /* atoms */
     const cta = z.object({
         text: z.string().min(1),
         href: z.string().min(1),
@@ -24,12 +25,14 @@ export const createPageSchema = ({ image }: Pick<SchemaContext, "image">) => {
         height: z.number().optional(),
         loading: z.enum(["lazy", "eager"]).optional(),
     });
+    /* atoms */
 
+    /* blocks */
     const hero = z.object({
         type: z.literal("hero"),
         layout: z.enum(["split", "centered"]).default("centered"),
         heading: z.string().min(1),
-        description: z.string().optional(),
+        description: z.string(),
         image: imageSchema.optional(),
         ctas: z.array(cta).optional(),
     });
@@ -38,12 +41,27 @@ export const createPageSchema = ({ image }: Pick<SchemaContext, "image">) => {
         type: z.literal("feature"),
         imagePosition: z.enum(["left", "right"]).default("left"),
         heading: z.string().min(1),
-        description: z.string().optional(),
+        description: z.string(),
         image: imageSchema.optional(),
         links: z.array(link).optional(),
     });
 
-    const blockSchema = z.discriminatedUnion("type", [hero, feature]);
+    const action = z.object({
+        type: z.literal("action"),
+        heading: z.string().min(1),
+        description: z.string(),
+        ctas: z.array(cta).optional(),
+    });
+
+    const testimonial = z.object({
+        type: z.literal("testimonial"),
+        heading: z.string().min(1),
+        description: z.string(),
+        caption: z.string(),
+    });
+    /* blocks */
+
+    const blockSchema = z.discriminatedUnion("type", [hero, feature, action, testimonial]);
 
     return z.object({
         title: z.string().min(1), // title & description are page metadata for SEO
